d570b1e74c8735c00af651b1461f1058f70e7969
Elizabeth Bauch <ebauch@mymail.mines.edu>
2022-12-16 04:37:11 -0700

    it eventaully stops when run with loss, wont run test script, timing is still an issue: possibly with the timeout function or the ertt.

--- GoBackN.cpp
+++ GoBackN.cpp
@@ -21,10 +21,10 @@ double ertta;
 // * entity A routines are called. You can use it to do any initialization
 // ***************************************************************************
 void A_init() {
-    base = 0;
-    nextSeq = 0;
-    expectedSeq = 0;
-    ertta = 1;
+    base = 1;
+    nextSeq = 1;
+    expectedSeq = 1;
+    ertta = 100; //???
 }
 
 // ***************************************************************************
@@ -32,43 +32,37 @@ void A_init() {
 // * entity B routines are called. You can use it to do any initialization
 // ***************************************************************************
 void B_init() {
-    expectedSeq = 0;
+    expectedSeq = 1;
 }
 
 // ***************************************************************************
 // * Called from layer 5, passed the data to be sent to other side 
 // ***************************************************************************
 bool rdt_sendA(struct msg message) {
+    
     // if(nextSeq - base >= 10){
     if (nextSeq >= (base + 10)) { //if window is full
+    //send data to upper layer???
         return false;
     }
     INFO << "RDT_SEND_A: Layer 4 on side A has received a message from the application that should be sent to side B: "
               << message << ENDL;
 
-    bool accepted = true;
         pkt newPkt;
-        newPkt.acknum = base - 1;
+        newPkt.acknum = 0;
         newPkt.seqnum = nextSeq;
-        // newPkt.checksum = 0;
         int cs = 0;
         for(int i = 0; i < 20; i++){
             newPkt.payload[i] = message.data[i];
             cs += int(message.data[i]);
         }
         newPkt.checksum = cs;
-        uninitialized.push_back(newPkt);
         simulation->udt_send(A, newPkt);
-        // if (base == nextSeq) {
-            simulation->start_timer(A, float(ertta*4));
-        // }
-        nextSeq ++;
-        // simulation->start_timer(A, float(ertta*4));
+        simulation->start_timer(A, ertta);
         sendTa = simulation->getSimulatorClock(); //RTT
- 
-     
-
-    return (accepted);
+        uninitialized.push_back(newPkt);
+        nextSeq ++;
+    return true;
 }
 
 
@@ -76,18 +70,23 @@ bool rdt_sendA(struct msg message) {
 // * Called from layer 3, when a packet arrives for layer 4 on side A
 // ***************************************************************************
 void rdt_rcvA(struct pkt packet) {
-    // int calcChecksum = atoi(packet.payload);
+    //is acknum equal to base, if not send packet[acknum + 1]
     int calcChecksum = 0;
         for(int i = 0; i < 20; i++){
             calcChecksum += int(packet.payload[i]);
         }
+    // if (packet.acknum != nextSeq - 1) {
+    //     simulation->start_timer(A, float(ertta*4));
+    //     simulation->udt_send(A, packet);
+        // sendTa = simulation->getSimulatorClock(); //RTT ???
+    // }
     // newPkt.checksum = cs;
     // if(packet.checksum == calcChecksum && packet.acknum >= base){
-    if(packet.checksum == calcChecksum){
+    if(packet.checksum == calcChecksum && packet.acknum >= base){
         recTa = simulation->getSimulatorClock();
-        base = packet.acknum + 1;
+        base = packet.acknum + 1; //previous packet successfully, move base up 1
         // if(base == nextSeq){
-            simulation->stop_timer(A);
+        simulation->stop_timer(A);
             ertta *= base;
             ertta += (recTa - sendTa);
             ertta /= base;
@@ -122,13 +121,24 @@ void rdt_rcvB(struct pkt packet) {
     bool noC = false;
     bool order = false;
     pkt newPkt;
-    newPkt.acknum = packet.acknum;
+    newPkt.acknum = packet.seqnum;
+    if(packet.seqnum == expectedSeq){
+        order = true;
+    }
+    else {
+        // newPkt.acknum = base - 1; //???
+                newPkt.acknum = base; //???
+        newPkt.seqnum = 0;
+    }
     if(packet.checksum == calcChecksum){
         noC = true;
-        newPkt.checksum = calcChecksum;
+        newPkt.checksum = calcChecksum; //???
     }
     else{
-        newPkt.acknum = base - 1;
+        // newPkt.acknum = packet.acknum - 1; //???
+        newPkt.acknum = base; //???
+        newPkt.seqnum = 0;
+
     }
 
     if(packet.seqnum == expectedSeq){
@@ -139,7 +149,7 @@ void rdt_rcvB(struct pkt packet) {
     msg newMsg;
     if(noC && order){
         // newPkt.seqnum = expectedSeq;
-        newPkt.acknum = packet.seqnum;
+        // newPkt.acknum = packet.seqnum;
         expectedSeq++;
         for(int i = 0; i < 20; i++){
             newMsg.data[i] = packet.payload[i];
@@ -153,7 +163,9 @@ void rdt_rcvB(struct pkt packet) {
 
         newPkt.checksum = cs;
         simulation->deliver_data(B, newMsg);
+        // newPkt.seqnum = 0;
     }
+    newPkt.seqnum = 0;
     simulation->udt_send(B, newPkt);
 }
 
@@ -162,10 +174,8 @@ void rdt_rcvB(struct pkt packet) {
 // ***************************************************************************
 void A_timeout() {
     INFO << "A_TIMEOUT: Side A's timer has gone off." << ENDL;
-    simulation->stop_timer(A);
     for(int i = base; i <= nextSeq - 1; i++){
         simulation->start_timer(A, ertta*4);
-        sendTa = simulation->getSimulatorClock();
         simulation->udt_send(A, uninitialized[i]);
     }
 }
